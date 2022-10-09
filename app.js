37fb694512b17b5e0d9fc0b5cfa2b58769510e9b
Nicolas Lain <lainnicolas1@gmail.com>
2022-10-09 18:12:02 -0300

    last deploy commit

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ app.use(express.static(publicPath))
 
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`)}
-    )
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`))
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
